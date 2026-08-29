070b2db3d477849b1ea2bb6dc81ec85cd6789517
laurent <lbertrand@groupe-mi.com>
2026-08-29 20:09:58 +0200

    fix: pass peer id to web client

--- frontend/src/utils/webclient.js
+++ frontend/src/utils/webclient.js
@@ -11,7 +11,7 @@ const app = useAppStore()
 
 export const toWebClientLink = (row) => {
   console.log(app.setting.rustdeskConfig)
-  window.open(`${app.setting.rustdeskConfig.api_server}/webclient/#/${row.id}`, '_blank', 'noopener,noreferrer')
+  window.open(`${app.setting.rustdeskConfig.api_server}/webclient/#/?id=${encodeURIComponent(row.id)}`, '_blank', 'noopener,noreferrer')
 }
 
 export async function getPeerSlat (id) {
